20c855f77997c61b9fa24bb0349ff33b790d0563
losyoguis <yoguisindevoz@gmail.com>
2020-04-09 14:16:08 -0500

    varios bloques

--- dev/test.js
+++ dev/test.js
@@ -2,5 +2,7 @@ const Blockchain = require('./blockchain');
 const bitcoin = new Blockchain();
 
 bitcoin.createNewBlock(2389, 'OJHJFJBJFBBFBNFBNFBMN', 'FGFJBFJKEJEJJEBJVEV');
+bitcoin.createNewBlock(56656, 'aaaOJHJFJBJFBBFBNFBNFBMN', 'bbbbbFGFJBFJKEJEJJEBJVEV');
+bitcoin.createNewBlock(9565, 'ccccOJHJFJBJFBBFBNFBNFBMN', 'dddddFGFJBFJKEJEJJEBJVEV');
 
 console.log(bitcoin);
