891296234540d042dd2da1a6b6716dca9fa35f65
losyoguis <yoguisindevoz@gmail.com>
2020-04-09 14:11:41 -0500

    Test primer bloque

--- dev/test.js
+++ dev/test.js
@@ -1,4 +1,6 @@
 const Blockchain = require('./blockchain');
 const bitcoin = new Blockchain();
 
+bitcoin.createNewBlock(2389, 'OJHJFJBJFBBFBNFBNFBMN', 'FGFJBFJKEJEJJEBJVEV');
+
 console.log(bitcoin);
