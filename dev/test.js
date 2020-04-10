68a51858cf788bc715dd6e1e6643e2fd720f4ba9
losyoguis <yoguisindevoz@gmail.com>
2020-04-10 10:41:14 -0500

    sha256

--- dev/test.js
+++ dev/test.js
@@ -3,6 +3,10 @@ const bitcoin = new Blockchain();
 
 bitcoin.createNewBlock(2389, '1JHJFJBJFBBFBNFBNFBMN', '2GFJBFJKEJEJJEBJVEV');
 
-bitcoin.createNewTransaction(100, 'yoguis111111', 'caliche2222222' )
+bitcoin.createNewTransaction(100, 'yoguis111111', 'caliche2222222');
+
+bitcoin.createNewBlock(0002, '3JHJFJBJFBBFBNFBNFBMN', '4GFJBFJKEJEJJEBJVEV');
+
+
 
 console.log(bitcoin);
