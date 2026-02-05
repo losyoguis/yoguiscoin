a8610ce666f238656bef86317cf7edfd3c2d8720
Juan Carlos Blandon Vargas <yoguisindevoz@gmail.com>
2026-02-04 23:36:15 -0500

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -2,5 +2,6 @@
 const STUDENT_NAME = "Estudiante Demo";
 const WALLET_ID = "YGC-10-02-001";
 
+
 // URL DEL WEB APP DE GOOGLE SHEETS
-const SHEETS_API = "PEGA_AQUI_TU_WEB_APP_URL";
+const SHEETS_API = "https://script.googleusercontent.com/a/macros/iemanueljbetancur.edu.co/echo?user_content_key=AehSKLi-dv7WIPTY_q0fuELGts7iF4jOhvkDPCxhI5rtWpMcLPQCpNbyzSDcAsEEOy_NE-LHboTe-sXkEHYCTFtUl3q2Zf0f4Yy76P4OmR2y-A_NyEypV6whvBhQi2DSxC2rgsPaVipM9QF3id6J89E6oqdqRN1KmNi4PEG6CDcvz5kSDGn7XDD8DW38I1WBSzITsIkCP98RyfNW7R4DBXNgcZJYfBsgkVwwuuIphU1XOSXC_mN0d5IuI8WO9tBpcSOQCsMVMFXnQsgABaOxlf6GQnkq9t5l1rdmKYEmzkmVLeKFYRkxSbbc_UaguZO6RFWNzVaxDn2t&lib=MEvfOuM0LVJWq9fNhE1N_Jt1dZNBdFPA6";
